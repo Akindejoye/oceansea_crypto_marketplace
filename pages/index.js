b01aa4c2fb702a4f9a224cda08cd49f7d7264432
Akindejoye <tydruhill@gmail.com>
2022-02-08 18:16:03 +0100

    Project Header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from './../components/Header';
 
 export default function Home() {
   return (
-    <h2>Money</h2>
+    <Header />
   )
 }
