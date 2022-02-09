105252fcd14025ffb258af053663e3271f110230
Akindejoye <tydruhill@gmail.com>
2022-02-09 12:11:57 +0100

    Adding lib directory, and logic for new user

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,62 @@
+import { useWeb3 } from '@3rdweb/hooks';
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react';
 import Hero from '../components/Hero';
 import styles from '../styles/Home.module.css'
 import Header from './../components/Header';
+import { client } from '../lib/sanityClient';
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
 
 export default function Home() {
+
+  const { address, connectWallet } = useWeb3();
+
+  useEffect(() => {
+    if (!address) return
+
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+      //Telling Sanity to create userDoc if user does'nt exist
+      const result = await client.createIfNotExists(userDoc)
+    })()
+
+  }, [address]);
+
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+
+    <div className={style.wrapper}>
+      {
+        address ? (
+          <>
+            <Header />
+            <Hero />
+          </>
+        ) : (
+          <div className={style.walletConnectWrapper}>
+            <button
+              className={style.button}
+              onClick={() => connectWallet('injected')}
+            >
+              Connect Wallet
+            </button>
+            <div className={style.details}>
+              You need Chrome to be<br />able to run this app.
+            </div>
+          </div>
+        )
+      }
+    </div>
   )
 }
